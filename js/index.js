14de6462e172d68919d613b890e013277aa45e21
Martseniuk Alina <martseniuk.av@gmail.com>
2024-10-22 17:52:13 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const age = 12;
+const name = "Alina";
+const isStudent = true;
+const myString = "";
+let myNumber = 7;
+// myNumber = myNumber + 10;
+myNumber += 10;
+let myNull = null;
+const mess = prompt("what your name");
+alert(`hello ${mess}`);
+const message = confirm("підтвердіть будь ласка");
+if (message) {
+alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+alert("попередженя!!!");
+const msg = confirm("дія небезпечна");
+if (msg) {
+alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+
+
+
+console.log(myString );
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(myNumber);
+console.log(myNull);
+console.log(mess);
+console.log(message);
+
+
